d84efd69ef16866eb46d329fabf152bf8b9d7951
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-07-26 21:13:13 +0900

    update churros_mapping churros_peakcall

---
+++ tutorial/02_peakcall.sh
@@ -0,0 +1,6 @@
+sing="singularity exec --bind /work,/work2 /work/SingularityImages/churros.0.1.0.sif"
+build=hg38
+Ddir=/work/Database/Database_fromDocker/Ensembl-GRCh38
+
+mkdir -p pdf
+$sing churros_peakcall samplepairlist.txt $build
