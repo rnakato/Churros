b97b919bbee3d47da3de125b055b68a46e44fa7c
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-07-30 16:52:16 +0900

    add churros

--- tutorial/03_visualize.sh
+++ tutorial/03_visualize.sh
@@ -6,4 +6,5 @@ mkdir -p pdf
 $sing churros_visualize samplepairlist.txt pdf/drompa+ $build $Ddir
 $sing churros_visualize macs/samplepairlist.txt pdf/drompa+.macspeak $build $Ddir
 $sing churros_visualize -b 5000 -l 8000 -P "--scale_tag 100" samplepairlist.txt pdf/drompa+.bin5M $build $Ddir
+$sing churros_visualize -p -b 5000 -l 8000 samplepairlist.txt pdf/drompa+.pval.bin5M $build $Ddir
 $sing churros_visualize -G macs/samplepairlist.txt pdf/drompa+ $build $Ddir
