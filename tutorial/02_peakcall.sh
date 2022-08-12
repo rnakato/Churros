641d34c653b28b1f583649257dcfd8682797eed7
Ryuichiro <rnakato@iqb.u-tokyo.ac.jp>
2022-08-12 17:42:23 +0900

    update churros_visualize

--- tutorial/02_peakcall.sh
+++ tutorial/02_peakcall.sh
@@ -1,4 +1,8 @@
 sing="singularity exec --bind /work,/work2 /work/SingularityImages/churros.0.1.0.sif"
 build=hg38
 
+
 $sing churros_callpeak -p 10 samplepairlist.txt $build
+
+# overwrite existing MACS results
+$sing churros_callpeak -F -p 10 samplepairlist.txt $build
