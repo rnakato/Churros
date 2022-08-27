91668dffebdd86720e0b7e6c9d2ef700068f5223
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-08-27 11:23:08 +0900

    add 05_genPvalue.sh

--- Dockerfiles/build.sh
+++ Dockerfiles/build.sh
@@ -1,6 +1,6 @@
 for tag in 0.2.0
 do
-    docker build -f Dokerfile.$tag -t rnakato/churros:$tag . #--no-cache
+    docker build -f Dockerfile.$tag -t rnakato/churros:$tag . #--no-cache
     docker push rnakato/churros:$tag
     docker tag rnakato/churros:$tag rnakato/churros:latest
     docker push rnakato/churros
