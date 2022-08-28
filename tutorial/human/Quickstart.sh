e1b9db3d416389a8154323a4d0b509893c314006
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-08-28 22:36:11 +0900

    v0.3.0

--- tutorial/human/Quickstart.sh
+++ tutorial/human/Quickstart.sh
@@ -1,5 +1,5 @@
-sing="singularity exec --bind /work,/work2 /work/SingularityImages/churros.0.3.0.sif"
-#sing="singularity exec churros.0.3.0.sif"
+#sing="singularity exec --bind /work,/work2 /work/SingularityImages/churros.0.3.0.sif"
+sing="singularity exec churros.0.3.0.sif"
 
 build=hg38
 Ddir=Ensembl-GRCh38/
