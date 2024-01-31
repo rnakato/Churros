98bacef768131f9d4ae407669ab5284b936b3e83
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2024-01-31 11:47:15 +0900

    Update v0.10.9

--- Dockerfiles/build.sh
+++ Dockerfiles/build.sh
@@ -2,7 +2,6 @@ tag=0.10.9
 docker build -f Dockerfile.$tag -t rnakato/churros:$tag . #--no-cache
 #docker save -o churros-$tag.tar rnakato/churros:$tag
 #singularity build -F churros.$tag.sif docker-archive://churros-$tag.tar
-exit
 
 docker push rnakato/churros:$tag
 docker tag rnakato/churros:$tag rnakato/churros:latest
