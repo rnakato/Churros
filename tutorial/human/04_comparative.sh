d30586ebde7fbd7283ef7ac9a9f75db3926ab629
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2024-07-20 15:11:05 +0900

    Update v1.1.1

--- tutorial/human/04_comparative.sh
+++ tutorial/human/04_comparative.sh
@@ -1,5 +1,5 @@
-sing="singularity exec --bind /work,/work2,/work3 /work3/SingularityImages/churros.1.1.1.sif"
-#sing="singularity exec churros.sif"
+#sing="singularity exec --bind /work,/work2,/work3 /work3/SingularityImages/churros.1.1.1.sif"
+sing="singularity exec churros.sif"
 
 build=hg38
 
