b4035c33596843d5de5ff4a0d323d249e20fc0c3
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2026-05-26 12:02:10 +0900

    Update tutorial/human/03_visualize.sh

--- tutorial/human/03_visualize.sh
+++ tutorial/human/03_visualize.sh
@@ -11,10 +11,10 @@ $sing churros_visualize samplepairlist.txt drompa+ $build $Ddir
 #$sing churros_visualize --pdfdir pdf_noinpput samplepairlist.noinput.txt drompa+ $build $Ddir
 
 $sing churros_visualize Churros_result/$build/macs/samplepairlist.txt drompa+.macspeak $build $Ddir
-$sing churros_visualize -b 5000 -l 8000 -P "--scale_tag 100" samplepairlist.txt drompa+.bin5M $build $Ddir
+$sing churros_visualize -b 5000 -l 8000 -P "--scale_tag 100" samplepairlist.txt drompa+.bin5k $build $Ddir
 
 # visualize -log10(p) distribution
-$sing churros_visualize --pvalue -b 5000 -l 8000 samplepairlist.txt drompa+.pval.bin5M $build $Ddir
+$sing churros_visualize --pvalue -b 5000 -l 8000 samplepairlist.txt drompa+.pval.bin5k $build $Ddir
 
 # Genome-wide view
 $sing churros_visualize -G Churros_result/$build/macs/samplepairlist.txt drompa+ $build $Ddir
